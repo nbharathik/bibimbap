60608176346ea8f4d2ca13602c183e6db41c743d
Bharathi <bharathikannan1311@gmail.com>
2026-01-19 11:09:58 +0100

    remove structured outputs that are not needed

--- data/structured_outputs/geom_output_16.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel, Field
-
-
-class ModelOutput(BaseModel):
-    deleted_guid: str = Field(description="GlobalId of the door that was deleted")
-    raw_model_response: str = Field(description="The raw model response")
